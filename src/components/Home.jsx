fc8cd3010e8c4fc2ccb62dadc15df9e6039b70db
Umolo Blessing Johnson <umoloblessing009@gmail.com>
2023-09-15 22:13:18 +0100

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../Assets/CSS/Style.css';
 import Logo from '../Assets/images/logo.svg'
 
 
 
-const firstText = [
-    "GadgetPLUG",
-    "Always Available for you",
-    "Anywhere you go"
-]
+// const firstText = [
+//     "GadgetPLUG",
+//     "Always Available for you",
+//     "Anywhere you go"
+// ]
 
 
 
@@ -41,45 +41,45 @@ const Home = () => {
     const [typingText, setTypingText] = useState('');
     const [isTyping, setIsTyping] = useState(true);
 
-    const [firstTextIndex, setfirstTextIndex] = useState(0);
-    const [firstTypingText, setfirstTypingText] = useState('');
-    const [firstIsTyping, setfirstIsTyping] = useState(true);
+    // const [firstTextIndex, setfirstTextIndex] = useState(0);
+    // const [firstTypingText, setfirstTypingText] = useState('');
+    // const [firstIsTyping, setfirstIsTyping] = useState(true);
 
 
-    useEffect(() => {
-      const firstCurrentText = firstText[firstTextIndex].split(" ");
-      let firstCurrentWordIndex = 0;
-      let firstCurrentWord = "";
+    // useEffect(() => {
+    //   const firstCurrentText = firstText[firstTextIndex].split(" ");
+    //   let firstCurrentWordIndex = 0;
+    //   let firstCurrentWord = "";
     
-      const typingInterval = setInterval(() => {
-        if (isTyping) {
-          if (firstCurrentWordIndex < firstCurrentText.length) {
-            firstCurrentWord += firstCurrentText[firstCurrentWordIndex] + " ";
-            setfirstTypingText(firstCurrentWord);
-            firstCurrentWordIndex++;
-          } else {
-            setfirstIsTyping(false);
-            setTimeout(() => {
-              setfirstIsTyping(true);
-              firstCurrentWordIndex = 0;
-              firstCurrentWord = "";
-              setfirstTextIndex((prevIndex) => (prevIndex + 1) % firstText.length);
-            }, 40000);
-          }
-        } else {
-          if (firstCurrentWord === "") {
-            setTimeout(() => {
-              setfirstTypingText("");
-            }, 5000);
-          } else {
-            firstCurrentWord = firstCurrentWord.slice(0, -1);
-            setfirstTypingText(firstCurrentWord);
-          }
-        }
-      }, 3000);
+    //   const typingInterval = setInterval(() => {
+    //     if (isTyping) {
+    //       if (firstCurrentWordIndex < firstCurrentText.length) {
+    //         firstCurrentWord += firstCurrentText[firstCurrentWordIndex] + " ";
+    //         setfirstTypingText(firstCurrentWord);
+    //         firstCurrentWordIndex++;
+    //       } else {
+    //         setfirstIsTyping(false);
+    //         setTimeout(() => {
+    //           setfirstIsTyping(true);
+    //           firstCurrentWordIndex = 0;
+    //           firstCurrentWord = "";
+    //           setfirstTextIndex((prevIndex) => (prevIndex + 1) % firstText.length);
+    //         }, 40000);
+    //       }
+    //     } else {
+    //       if (firstCurrentWord === "") {
+    //         setTimeout(() => {
+    //           setfirstTypingText("");
+    //         }, 5000);
+    //       } else {
+    //         firstCurrentWord = firstCurrentWord.slice(0, -1);
+    //         setfirstTypingText(firstCurrentWord);
+    //       }
+    //     }
+    //   }, 3000);
     
-      return () => clearInterval(typingInterval);
-    }, [firstTextIndex, firstIsTyping]);
+    //   return () => clearInterval(typingInterval);
+    // }, [firstTextIndex, firstIsTyping]);
 
     
 
@@ -136,7 +136,7 @@ const Home = () => {
                 </h1>
 
                 <div className={`text-[35px] xl:text-[50px] font-[700] font-['Lily_Script_One'] text-[#FF7576] mt-[150px] md:mt-[300px] xl:mt-[350px] 2xl:mt-[300px] px-[20px] ${isTyping ? 'typing-animation' : ''}`}>
-                  {firstTypingText}
+                  {/* {firstTypingText} */}
                   <div>
                   {typingText} 
                   {isTyping && <div className="typing-circle">
